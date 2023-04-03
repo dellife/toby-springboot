be35b5e0385e56f436c072a3a95ee7284462a424
dellife <seheekwon21@gmail.com>
2023-04-03 22:44:00 +0900

    first commit1

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,48 @@
+package tobyspring.helloboot;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@SpringBootApplication
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+
+//		SpringApplication.run(HellobootApplication.class, args);
+
+//		spring 도움없이 띄워보자
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+		//tomcat에 국한되지 않고 추상화 시킨 WebServer
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
+				}
+			}).addMapping("/hello");
+
+
+		});
+		webServer.start();
+	}
+
+}
+
+
